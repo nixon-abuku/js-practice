24024d88c8df1b0af51bd5f52a71c78825b7caee
Nixon Abuku <nixonmatara91@gmail.com>
2026-05-07 11:53:22 -0400

    a simple js calculator code that calculates 10 and 5

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+const addition = add (10, 5);
+const subtraction = subtract (10, 5);
+const multiplication = multiply(10, 5);
+const division = divide(10, 5);
+
+console.log(`10 + 5 = ${addition}`);
+console.log(`10 - 5 = ${subtraction}`);
+console.log(`10 * 5 = ${multiplication}`);
+console.log(`10 / 5 = ${division}`);
