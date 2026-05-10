d8c1b939d4fb1b28a82f034eb2ac6ca9067e6a01
Nixon Abuku <nixonmatara91@gmail.com>
2026-05-10 11:48:13 -0400

    add day5.js, using .filter() and .map()

--- day5.js
+++ day5.js
@@ -6,6 +6,25 @@
 // 1. create a variable to store hours worked in shift  using let 
 // 2. Use if statement to tell if the shift was long or normal
 
+const myShifts = [
+    {day: "Sunday", hrs: 10},
+    {day: "Monday", hrs: 10},
+    {day: "Tuesday", hrs: 8},
+    {day: "Wednesday", hrs: 12}
+]
+const longShifts = myShifts.filter(function(shift){
+    return shift.hrs >= 10;
+
+});
+const daysWorked = myShifts.map(function(shift){
+    return shift.day;
+});
+console.log(daysWorked);
+console.log(longShifts);
+
+
+//Why didnt we do function Myshifts(shift)
+/*
 let hrsWorked = 6;
 if (hrsWorked > 8){
     console.log(`The shift was ${hrsWorked} hrs so it was a long shift`)
@@ -14,4 +33,4 @@ if (hrsWorked > 8){
 }
 else{
     console.log(`The shift was ${hrsWorked} hrs so it was a normal shift`)
-}
+}*/
